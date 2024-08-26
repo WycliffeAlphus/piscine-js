be416b0807c4bc112c9446f03adb626b0e1ae8b2
wyonyango <wycliffealphus7@gmail.com>
2024-08-26 18:13:15 +0300

    feat:add method-man file

---
+++ method-man.js
@@ -0,0 +1,21 @@
+function words(n){
+return n.split("");
+}
+
+function sentence(n){
+return n.join(" ")
+}
+function yell(n){
+    return n.toUpperCase()
+}
+
+function whisper(n){
+    
+    return "*"+n.toLowerCase()+"*";
+}
+
+function capitalize(n){
+    let first = n.charAt(0).toUpperCase();
+    let second = n.Slice(1).toLowerCase();
+    return first+second
+}
