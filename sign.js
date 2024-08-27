08a04b1e5fad93e5fbcfeec0d2b3800b85439975
wyonyango <wycliffealphus7@gmail.com>
2024-08-27 09:11:43 +0300

    add sign file

---
+++ sign.js
@@ -0,0 +1,13 @@
+function sign(n){
+    if (n>0){
+        return 1
+    } else if (n<0){
+        return -1
+    } else {
+        return 0
+    }
+}
+
+function sameSign(a,b){
+    return a===b?true:false
+}
