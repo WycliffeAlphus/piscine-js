84a29cd50e25fa34390b7f24c145d7c0b789f6c3
wyonyango <wycliffealphus7@gmail.com>
2024-08-27 11:19:34 +0300

    feat:add getAcceleration file

---
+++ physics.js
@@ -0,0 +1,13 @@
+
+
+function getAcceleration(n){
+    if (typeof n[f]==='number' && typeof n[m] === 'number'){
+        return n[f]/n[m];
+    } if (typeof n[Δv]==='number' && typeof n[Δt] === 'number'){
+        return n[Δv]/n[Δt];
+    } if (typeof n[t]==='number' && typeof n[d] === 'number'){
+        return (2*(n[d]))/(n[t]*n[t]);
+    } else {
+        return "impossible"
+    }
+}
