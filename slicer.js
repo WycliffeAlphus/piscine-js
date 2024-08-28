74f069af972eff4f3a97cb5d5e8646aeb7c51a69
wyonyango <wycliffealphus7@gmail.com>
2024-08-28 16:06:39 +0300

    feat:add slicer

---
+++ slicer.js
@@ -0,0 +1,21 @@
+// slice works like Array.slice() and String.slice().
+function slice(a,b, c){
+if (typeof c=='undefined'){
+    c=a.length
+}
+if (typeof a === 'string'){
+    let result ="";
+    for (let i=b; i<c; i++){
+      result+=a[i]  
+    }
+    return result;
+}
+if (typeof Array.isArray(a)){
+    let result =[];
+    for (let i=b; i<c; i++){
+      result.push(a[i]); 
+    }
+    return result;
+}
+}
+
