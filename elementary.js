44116721182f20757880b01f5d82c675124c0845
wyonyango <wycliffealphus7@gmail.com>
2024-08-28 14:40:42 +0300

    feat:add elementary file

--- elementary.js
+++ elementary.js
@@ -1,7 +1,21 @@
 function multiply(a, b){
     let result = 0;
+    let sign = false;
+    if (a < 0 && b < 0) {
+        a *= -1;
+        b *= -1;
+    } else if (a < 0 && b > 0){
+        a *=-1;
+        sign = true;
+    } else if ( a>0 && b < 0){
+        b *=-1;
+        sign = true;
+    }
 for (let i =0; i<b;i++){
     result +=a
+}
+if (sign){
+    result *=-1
 }
 return result
 }
