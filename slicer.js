a459d0928e747375a9792c5b070dbbbed6b94000
wyonyango <wycliffealphus7@gmail.com>
2024-08-28 16:10:10 +0300

    feat:add slicer

--- slicer.js
+++ slicer.js
@@ -3,6 +3,9 @@ function slice(a,b, c){
 if (typeof c=='undefined'){
     c=a.length
 }
+if (b<0){
+    b = a.length+b
+}
 if (typeof a === 'string'){
     let result ="";
     for (let i=b; i<c; i++){
