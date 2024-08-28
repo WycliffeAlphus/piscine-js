89dd868f98600eef51c213371fe819db5ac0ec6b
wyonyango <wycliffealphus7@gmail.com>
2024-08-28 15:31:50 +0300

    feat:add indec-of file

--- index-of.js
+++ index-of.js
@@ -13,9 +13,11 @@ return -1;
 }
 
 // lastIndexOf returns the index of the last occurrence
-function lastIndexOf(a, b){
-    
-     for (let i=a.length; i>=0; i--){
+function lastIndexOf(a, b, c){
+    if (typeof c==='undefined'){
+        c = a.length;
+     }
+     for (let i=c; i>=0; i--){
          if (a[i] === b) {
              return i;
          }
@@ -24,8 +26,11 @@ function lastIndexOf(a, b){
 }
 
 // includes returns true if te value was found in the array, and false otherwise
-function includes(a,b){
-    for (let i=0; i<a.length; i++){
+function includes(a,b,c){
+    if (typeof c==='undefined'){
+        c = 0;
+     }
+    for (let i=c; i<a.length; i++){
         if (a[i] === b) {
             return true;
         }
