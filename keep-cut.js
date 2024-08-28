06a038762355f18f32179e059ca261787e4b4ecb
wyonyango <wycliffealphus7@gmail.com>
2024-08-28 17:13:50 +0300

    feat:add keep-cut

---
+++ keep-cut.js
@@ -0,0 +1,74 @@
+//cutFirst: returns the string with the first 2 characters removed.
+function cutFirst(n){
+let res ="";
+for (let i=0;i<n.length;i++){
+    if (i > 1){
+        res+=n[i];
+    }
+}
+return res;
+}
+
+function cutLast(n){
+    let res ="";
+    for (let i=n.length-1;i>=0;i--){
+        if (i < n.length-2){
+            res+=n[i];
+        }
+    }
+    return res;
+}
+
+function cutFirstLast(n){
+    let res ="";
+for (let i=0;i<n.length;i++){
+    if (i > 1){
+        res+=n[i];
+    }
+}
+for (let i=n.length-1;i>=0;i--){
+    if (i < n.length-2){
+        res+=n[i];
+    }
+}
+return res;
+}
+
+function keepFirst(n){
+    let res ="";
+    for (let i=0;i<n.length;i++){
+        res+=n[i]
+        if (i==1){
+           return res;
+        }
+    }
+    return res;
+}
+
+function keepLast(n){
+    let res = "";
+    for (let i=n.length;i>=0;i--){
+        res+=n[i];
+        if (i == n.length-2){
+            return res;
+        }
+    }
+    return res;
+}
+
+function keepFirstLast(n){
+    let res ="";
+for (let i=0;i<n.length;i++){
+    res+=n[i];
+    if (i > 1){
+        break;
+    }
+}
+for (let i=n.length-1;i>=0;i--){
+    res+=n[i]
+    if (i < n.length-2){
+        break;
+    }
+}
+return res;
+}
