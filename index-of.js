82ce81743c95c611cefa9702bfd1cd4b6c3847f6
wyonyango <wycliffealphus7@gmail.com>
2024-08-28 15:24:08 +0300

    feat:add indec-of file

---
+++ index-of.js
@@ -0,0 +1,35 @@
+//indexOf accept an array to be searched, and a value to be matched, returns the index of the first occurrence. 
+function indexOf(a, b, c){
+if (typeof c=='undefined'){
+   c = 0;
+}
+
+for (let i=c; i<a.length; i++){
+    if (a[i] === b) {
+        return i;
+    }
+}
+return -1;
+}
+
+// lastIndexOf returns the index of the last occurrence
+function lastIndexOf(a, b){
+    
+     for (let i=a.length; i>=0; i++){
+         if (a[i] === b) {
+             return i;
+         }
+     }
+     return -1;
+}
+
+// includes returns true if te value was found in the array, and false otherwise
+function includes(a,b){
+    for (let i=0; i<a.length; i++){
+        if (a[i] === b) {
+            return true;
+        }
+    }
+    return false;
+}
+
