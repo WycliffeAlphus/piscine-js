c25cd2815f7ccbd85d7a61e4ed1319984dee540f
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 17:48:09 +0300

    add reverse

--- reverser.js
+++ reverser.js
@@ -11,9 +11,8 @@ if (Array.isArray(nb)){
 if (typeof nb==='string'){
     let newArray1 = "";
     for (let i=0;i<nb.length; i++){
-        newArray1 = nb[i]+newArray
+        newArray1 = nb[i]+newArray1
     }
     return newArray1
 }
 }
-
