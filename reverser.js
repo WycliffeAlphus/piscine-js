4630fab7af790f204c387b9ea2a98241bcc258ce
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 17:35:25 +0300

    add reverse

---
+++ reverser.js
@@ -0,0 +1,15 @@
+function reverse(nb){
+let newArray = [];
+if (Array.isArray(nb)){
+    for (let i=0;i<length(nb); i++){
+        newArray.unshift(nb[i])
+    }
+}
+let newArray1 = "";
+if (typeof n==='string'){
+    for (let i=0;i<length(nb); i++){
+        newArray1 = nb[i]+newArray
+    }
+    return newArray1
+}
+}
