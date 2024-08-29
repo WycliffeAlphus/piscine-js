fba0245eb769b1c5614210384d41191a2b976ec2
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 18:14:01 +0300

    feat:add unbreakable

---
+++ unbreakable.js
@@ -0,0 +1,34 @@
+function split(a, b){
+if (typeof b==='undefined'){
+    return [a]
+}
+let newS = [];
+let temp = "";
+for (let i=0; i<a.length; i++){
+    if (a[i] === b){
+        newS.push(temp)
+        temp = ""
+        continue
+    }
+    temp+=a[i]
+}
+return newS
+}
+
+
+function join(a,b){
+    if (typeof b==='undefined'){
+        b = ','
+    }
+    let nString = "";
+  
+    for (let i=0; i < a.length;i++){
+
+        if (nString.length===0){
+            nString+=a[i]
+        } else {
+            nString += b+a[i]
+        }
+    }
+    return nString
+}
