4fa5d4b1a4b3c8ea18955ca6e0ea24bae6857cfb
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 14:38:50 +0300

    feat:add cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,24 +1,72 @@
-function round(nb){
-    const nbDec = nb-parseInt(nb);
-    if (nbDec >=0.5) {
-        return parseInt(nb)+1;
-    } else if (nbDec < 0.5) {
-        return parseInt(nb);
-    } else {
-        return parseInt(nb);
+// round behaves similar to Math.round()
+function round(nb) {
+    nb = Number(nb)
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
+        return 0
     }
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
+    }
+    let firstPart = 0;
+    for (; firstPart <= nb - 1; ) {
+        firstPart++
+    }
+    let result = (nb - firstPart) >= 0.5 ? firstPart + 1 : firstPart;
+    return sign ? -result : result;
 }
 
-function ceil(nb){
-    const firstPart = parseInt(nb);
-    return (nb>firstPart)?firstPart+1:nb;
+// ceil behaves similar to Math.ceil()
+function ceil(nb) {
+    nb = Number(nb)
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
+        return 0
+    }
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
+    }
+    let firstPart = 0;
+    for (; firstPart < nb; ) {
+        firstPart++
+    }
+
+    return sign ? -(firstPart > nb ? firstPart - 1 : firstPart) : firstPart;
 }
 
-function floor(nb){
-    const firstPart = parseInt(nb);
-    return(nb>firstPart)?firstPart-1:nb;
+// floor behaves similar to Math.floor()
+function floor(nb) {
+    nb = Number(nb)
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
+        return 0
+    }
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
+    }
+    let firstPart = 0;
+    for (; firstPart < nb - 1; ) {
+        firstPart++
+    }
+
+    return sign ? -(firstPart + (firstPart < nb ? 1 : 0)) : firstPart;
 }
 
-function trunc(nb){
-    return parseInt(nb);
+
+// trunc behaves similar to Math.trunc()
+function trunc(nb) {
+    nb = Number(nb)
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
+        return 0
+    }
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
+    }
+    let firstPart = 0;
+    for (; firstPart < nb - 1; ) {
+        firstPart++
+    }
+
+    return sign ? -firstPart : firstPart;
 }
