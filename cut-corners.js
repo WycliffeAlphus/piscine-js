f383271abaf2b38b41e7a34bf316b6701fa08dc8
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 16:33:41 +0300

    feat:add cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,72 +1,82 @@
-// round behaves similar to Math.round()
+// Custom modulo function that acts like the % operator, without using it.
+function modulo(a, b) {
+    let sign = false;
+    if (a < 0 && b < 0) {
+        a = -a;
+        b = -b;
+        sign = true;
+    } else if (a < 0 && b > 0) {
+        a = -a;
+        sign = true;
+    } else if (a > 0 && b < 0) {
+        b = -b;
+    }
+    let num = a;
+    while (num >= b) {
+        num -= b;
+    }
+    if (sign) {
+        num = -num;
+    }
+    return num;
+}
+
+// round function behaves similar to Math.round()
 function round(nb) {
-    nb = Number(nb)
-    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
-        return 0
+    nb = Number(nb);
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER) {
+        return 0;
     }
     let sign = nb < 0;
     if (sign) {
         nb = -nb;
     }
-    let firstPart = 0;
-    for (; firstPart <= nb - 1; ) {
-        firstPart++
-    }
-    let result = (nb - firstPart) >= 0.5 ? firstPart + 1 : firstPart;
+    let intPart = nb - modulo(nb, 1);
+    let fractionalPart = nb - intPart;
+    let result = fractionalPart >= 0.5 ? intPart + 1 : intPart;
     return sign ? -result : result;
 }
 
-// ceil behaves similar to Math.ceil()
+// ceil function behaves similar to Math.ceil()
 function ceil(nb) {
-    nb = Number(nb)
-    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
-        return 0
+    nb = Number(nb);
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER) {
+        return 0;
     }
     let sign = nb < 0;
     if (sign) {
         nb = -nb;
     }
-    let firstPart = 0;
-    for (; firstPart < nb; ) {
-        firstPart++
-    }
-
-    return sign ? -(firstPart > nb ? firstPart - 1 : firstPart) : firstPart;
+    let intPart = nb - modulo(nb, 1);
+    let result = modulo(nb, 1) > 0 ? intPart + 1 : intPart;
+    return sign ? -(result-1) : result;
 }
 
-// floor behaves similar to Math.floor()
+// floor function behaves similar to Math.floor()
 function floor(nb) {
-    nb = Number(nb)
-    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
-        return 0
+    nb = Number(nb);
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER) {
+        return 0;
     }
     let sign = nb < 0;
     if (sign) {
         nb = -nb;
     }
-    let firstPart = 0;
-    for (; firstPart < nb - 1; ) {
-        firstPart++
-    }
-
-    return sign ? -(firstPart + (firstPart < nb ? 1 : 0)) : firstPart;
+    let intPart = nb - modulo(nb, 1);
+    return sign ? -(intPart + (modulo(nb, 1) > 0 ? 1 : 0)) : intPart;
 }
 
-
-// trunc behaves similar to Math.trunc()
+// trunc function behaves similar to Math.trunc()
 function trunc(nb) {
-    nb = Number(nb)
-    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER){
-        return 0
+    nb = Number(nb);
+    if (nb === 0 || nb > Number.MAX_SAFE_INTEGER || nb < Number.MIN_SAFE_INTEGER) {
+        return 0;
     }
     let sign = nb < 0;
     if (sign) {
         nb = -nb;
     }
-    let firstPart = 0;
-    for (; firstPart < nb - 1; ) {
-        firstPart++
-    }
-
-    return sign ? -firstPart : firstPart;
+    let intPart = nb - modulo(nb, 1);
+    return sign ? -intPart : intPart;
 }
+
