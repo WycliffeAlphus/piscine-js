79855dc1f24530bc0fd4b416aa44ff20ddea8b86
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 12:50:22 +0300

    feat:add cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -62,8 +62,3 @@ function trunc(nb) {
 
     return sign ? -firstPart : firstPart;
 }
-const nums = [3.7, -3.7, 3.1, -3.1]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
