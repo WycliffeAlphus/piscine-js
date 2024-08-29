ba43ed1c2afc82cb6fa9e2267949022a8e8d863e
wyonyango <wycliffealphus7@gmail.com>
2024-08-29 09:54:03 +0300

    feat:add cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,81 +1,73 @@
 // round behaves similar to Math.round()
 function round(nb) {
-
-    if (nb <= nb.MAX_SAFE_INTEGER){
-        let sign = nb < 0;
-        if (sign) {
-            nb = -nb;
-        }
-    
-        let firstPart = 0;
-        while (firstPart + 1 <= nb) {
-            firstPart++;
-        }
-    
-        let result = (nb - firstPart) >= 0.5 ? firstPart + 1 : firstPart;
-        return sign ? -result : result;
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
     }
-  
+
+    let firstPart = 0;
+    while (firstPart + 1 <= nb) {
+        firstPart++;
+    }
+
+    let result = (nb - firstPart) >= 0.5 ? firstPart + 1 : firstPart;
+    return sign ? -result : result;
 }
 
 // ceil behaves similar to Math.ceil()
 function ceil(nb) {
-
-    if (nb <= nb.MAX_SAFE_INTEGER){
-        let sign = nb < 0;
-        if (sign) {
-            nb = -nb;
-        }
-    
-        let firstPart = 0;
-        while (firstPart < nb) {
-            firstPart++;
-        }
-    
-        if (sign) {
-            return -(firstPart > nb ? firstPart - 1 : firstPart);
-        }
-    
-        return firstPart;
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
     }
-  
+
+    let firstPart = 0;
+    while (firstPart < nb) {
+        firstPart++;
+    }
+
+    if (sign) {
+        return -(firstPart > nb ? firstPart - 1 : firstPart);
+    }
+
+    return firstPart;
 }
 
 // floor behaves similar to Math.floor()
 function floor(nb) {
-    if (nb <= nb.MAX_SAFE_INTEGER){
-        let sign = nb < 0;
-        if (sign) {
-            nb = -nb;
-        }
-    
-        let firstPart = 0;
-        while (firstPart + 1 <= nb) {
-            firstPart++;
-        }
-    
-        if (sign) {
-            return -(firstPart + (firstPart < nb ? 1 : 0));
-        }
-    
-        return firstPart;
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
     }
 
+    let firstPart = 0;
+    while (firstPart + 1 <= nb) {
+        firstPart++;
+    }
+
+    if (sign) {
+        return -(firstPart + (firstPart < nb ? 1 : 0));
+    }
+
+    return firstPart;
 }
 
 // trunc behaves similar to Math.trunc()
 function trunc(nb) {
-    if (nb <= nb.MAX_SAFE_INTEGER){
-        let sign = nb < 0;
-        if (sign) {
-            nb = -nb;
-        }
-    
-        let firstPart = 0;
-        while (firstPart + 1 <= nb) {
-            firstPart++;
-        }
-    
-        return sign ? -(firstPart) : firstPart;
+    let sign = nb < 0;
+    if (sign) {
+        nb = -nb;
     }
+
+    let firstPart = 0;
+    while (firstPart + 1 <= nb) {
+        firstPart++;
+    }
+
+    return sign ? -(firstPart) : firstPart;
 }
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round))
+console.log(nums.map(floor))
+console.log(nums.map(trunc))
+console.log(nums.map(ceil))
