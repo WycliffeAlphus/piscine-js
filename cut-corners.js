b98818da88d106cc0883d5358a99d1fe7ba78bdd
Wycliffe Onyango <wyonyango@noreply.learn.zone01kisumu.ke>
2024-08-29 02:07:23 +0000

    Update 'cut-corners.js'

--- cut-corners.js
+++ cut-corners.js
@@ -1,9 +1,17 @@
-function round(nb){
 // round behaves similar to Math.round()
+function round(nb){
+           let sign = false;
+    if(nb<0) {
+        sign=true;
+        nb = -nb;
+    }
 if (nb<0) return -round(-nb);
   let firstPart = 0;
   while(firstPart+1 <= nb){
       firstPart++
+  }
+  if (sign){
+      return -((nb-firstPart)>0.5?firstPart-1:firstPart);
   }
   return (nb-firstPart)>0.5?firstPart+1:firstPart;
 }
@@ -20,7 +28,7 @@ function ceil(nb){
         firstPart++
     }
       if (sign){
-        return -firstPart
+        return -(firstPart-(firstPart>nb?1:0));
     }
     return firstPart;
 }
@@ -53,7 +61,7 @@ function trunc(nb){
     firstPart++
     }
     if (sign){
-        return -firstPart
+        return -firstPart;
     }
     return firstPart;
 }
