964ddf97dd55268473b44ec253cc35d6b4fe7080
wyonyango <wycliffealphus7@gmail.com>
2024-08-30 15:41:02 +0300

    feat:add get file

--- get.js
+++ get.js
@@ -3,10 +3,10 @@ function get(src, path){
 let keysArr = path.split(".")
 let current = src;
 for (let i=0; i < keysArr.length; i++){
-    if ( current && typeof src==='object' && keysArr[i] in current){
+    if ( current && typeof current==='object' && keysArr[i] in current){
         current = current[keysArr[i]];
     } else {
-        return 'undefined'
+        return undefined;
     }
     }
   
