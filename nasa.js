3198d0978fc35e6cb189bb2e18def2d994147eaf
wyonyango <wycliffealphus7@gmail.com>
2024-08-30 14:41:37 +0300

    feat:add nasa file

--- nasa.js
+++ nasa.js
@@ -6,11 +6,11 @@ for (let i=1; i<=n; i++){
     } else if (i%3===0){
        res.push("NA")
     } else if (i%5===0){
-       res.push("NASA");
+       res.push("SA");
     } else {
           res.push(i.toString());
 }
 }
 return res.join(" ");
 }
-console.log(nasa(15))
+
