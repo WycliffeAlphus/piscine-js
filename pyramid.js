49cd7343cd2cefc465a8062d05539794bd087a0d
wyonyango <wycliffealphus7@gmail.com>
2024-08-30 17:25:05 +0300

    feat: add pyramid feature

---
+++ pyramid.js
@@ -0,0 +1,18 @@
+function pyramid(a, b) {
+    let res = "";
+    for (let i = 0; i < b; i++) {
+     
+        const spaces = ' '.repeat(b - i - 1); //the leading spaces
+        // Generate the current row of the pyramid
+        const row = a.repeat(2 * i + 1);
+       
+        if (i < b-1){
+            res += spaces + row + "\n";
+        } else {
+            res += spaces + row
+        }
+        
+    }
+    return res;
+}
+
