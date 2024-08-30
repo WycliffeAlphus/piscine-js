0f1b86adf45a723b0aa9367088785c79ef24ef8f
wyonyango <wycliffealphus7@gmail.com>
2024-08-30 16:36:58 +0300

    feat:add get file

--- get.js
+++ get.js
@@ -2,16 +2,19 @@ function get(src, path) {
     let keysArr = path.split(".");
     let current = src;
     for (let i = 0; i < keysArr.length; i++) {
-      if (current && typeof current === 'object') {
-        const key = keysArr[i];
+      if (current == null) {  // null or undefined
+        return undefined;
+      }
+      const key = keysArr[i];
+      if (Array.isArray(current)) {
         const index = parseInt(key, 10);
-        if (!isNaN(index) && Array.isArray(current)) {
+        if (!isNaN(index)) {
           current = current[index];
-        } else if (key in current) {
-          current = current[key];
-        } else {
-          return undefined;
+          continue;
         }
+      }
+      if (typeof current === 'object' || typeof current === 'function') {
+        current = current[key];
       } else {
         return undefined;
       }
