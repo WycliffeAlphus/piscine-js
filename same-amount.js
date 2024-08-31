01e9367451aa26c1c96ddeafbc14a18acec3d8d1
Wycliffe Onyango <wyonyango@noreply.learn.zone01kisumu.ke>
2024-08-31 02:50:54 +0000

    Update 'same-amount.js'

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,7 @@
 function sameAmount(a, b, c){
     let firstMatch = a.match(b);
     let secondMatch = a.match(c);
-    return (firstMatch.length===secondMatch.length)?true:false;
+    firstMatch = firstMatch || []
+    secondMatch = secondMatch || []
+    return firstMatch.length===secondMatch.length;
 }
