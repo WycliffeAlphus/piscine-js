3a519b145a71dc2f4c11d3be70a7d65ad6990b05
wyonyango <wycliffealphus7@gmail.com>
2024-09-03 15:27:27 +0300

    feat:add get-some-time file

--- get-some-time.js
+++ get-some-time.js
@@ -1,29 +1,32 @@
-function firstDayWeek(weekNumber, year) {
-    
-    let firstDay = new Date(year, 0, 1);
-    
-
-    let dayOfWeek = firstDay.getDay();
-
-    let firstMonday;
-    
-   
-    if (dayOfWeek === 0) {
-        firstMonday = new Date(year, 0, 2); 
-    } else if (dayOfWeek === 1) {
-        firstMonday = new Date(year, 0, 1); 
-    } else if (dayOfWeek > 1) {
-        firstMonday = new Date(year, 0, 1 + (8 - dayOfWeek)); 
+function firstDayWeek(week, year) {
+    let time = new Date(year);
+    if (week === 1) {
+        time.setHours(24);
+        return formattedDate(time);
     }
-
-
-    let startOfWeek = new Date(firstMonday);
-    startOfWeek.setDate(firstMonday.getDate() + (weekNumber - 1) * 7);
-
-    let day = startOfWeek.getDate().toString().padStart(2, '0');
-    let month = (startOfWeek.getMonth() + 1).toString().padStart(2, '0');
-    let formattedDate = `${day}-${month}-${startOfWeek.getFullYear()}`;
-
-    return formattedDate;
+    
+    let dayPlus = week * 7 * 24;
+    time.setHours(dayPlus - 123);
+    
+    for (let i = 0; i < 7; i++) {
+        if (getWeekDay(time) === 'Monday') {
+            return formattedDate(time);
+        }
+        time.setHours(-24);
+    }
+    return time;
 }
 
+function getWeekDay(date) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay() - 1];
+}
+
+function formattedDate(d) {
+    let month = String(d.getMonth() + 1).padStart(2, '0');
+    let day = String(d.getDate() - 1).padStart(2, '0');
+    let year = String(d.getFullYear()).padStart(4, '0');
+    return `${day}-${month}-${year}`;
+}
+
+console.log(firstDayWeek(1, 1000))
