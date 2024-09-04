a15be12bb4bffa906d298bdc82f680d5217d7f96
Wycliffe Onyango <wyonyango@noreply.learn.zone01kisumu.ke>
2024-09-04 01:57:26 +0000

    Update 'count-leap-years.js'

--- count-leap-years.js
+++ count-leap-years.js
@@ -4,7 +4,7 @@ function countLeapYears(date) {
   
    
     for (let i = 1; i <= year; i++) {
-        if (i % 4 === 0 && (i % 100 !== 0 || i % 400 === 0)) {
+        if (i % 4 === 0) {
             count++;
         }
     }
