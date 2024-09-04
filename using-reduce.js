516425a3c4e1fd9caff99191cdfd6242d2bdb053
wyonyango <wycliffealphus7@gmail.com>
2024-09-04 14:45:30 +0300

    add reduce

--- using-reduce.js
+++ using-reduce.js
@@ -6,6 +6,10 @@ function adder(arr, initialValue = 0) {
 }
 
 function sumOrMul(arr, initialValue) {
+    if (initialValue === undefined) {
+        initialValue = arr[0];
+        arr = arr.slice(1); // Remove the first element from the array
+    }
     return arr.reduce(
         (accumulator, currentValue) => {
             if (currentValue % 2 === 0) {
