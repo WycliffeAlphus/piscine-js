159efee79fb0487429b85522489574ac9150f12f
wyonyango <wycliffealphus7@gmail.com>
2024-09-04 15:34:21 +0300

    feat:add using-maps

---
+++ long-words.js
@@ -0,0 +1,11 @@
+function longWords(arr){
+  return arr.every((elm)=>typeof elm==='string'&&elm.length>=5)   
+}
+
+function oneLongWord(arr){
+    return arr.some((elm)=>typeof elm==='string'&&elm.length>=10)
+}
+
+function noLongWords(arr){
+    return !arr.every((elm=>typeof elm ==='string'&&elm.length>=7))
+}
