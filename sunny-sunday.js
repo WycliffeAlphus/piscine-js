041df79dec7411d2e00c0560662a26d08b6fd208
Wycliffe Onyango <wyonyango@noreply.learn.zone01kisumu.ke>
2024-09-04 03:19:16 +0000

    Update 'sunny-sunday.js'

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,7 +1,7 @@
 function sunnySunday(date){
 const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-const start = new Date('0001-01-01T00:00:00Z');
- const daysSinceStart = Math.floor((date.getTime() - start.getTime()) / (24 * 60 * 60 * 1000));
- const weekdayIndex = (daysSinceStart+1)%6;
+const epoch = new Date('0001-01-01');
+ const days = Math.floor((date-epoch) / (24 * 60 * 60 * 1000));
+ const weekdayIndex = days % 6;
  return weekdays[weekdayIndex];
 }
