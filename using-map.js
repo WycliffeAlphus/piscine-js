f3b8dc84df75be3053e72927e66c685f78e5f35c
wyonyango <wycliffealphus7@gmail.com>
2024-09-04 15:33:31 +0300

    feat:add using-maps

--- using-map.js
+++ using-map.js
@@ -1,35 +1,35 @@
-function citiesOnly(objArray){
-    return objArray.map((obj)=>obj.city);
+function citiesOnly(objArray) {
+    return objArray.map((obj) => obj.city);
 }
 
-function upperCasingStates(objArray){
-    return objArray.map((str)=>{
+function upperCasingStates(objArray) {
+    return objArray.map((str) => {
         return str
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+            .join(' ');
     });
 }
 
-function fahrenheitToCelsius(objArray){
-    return objArray.map(fahrenheit=>{
+function fahrenheitToCelsius(objArray) {
+    return objArray.map(fahrenheit => {
         const value = parseFloat(fahrenheit.trim());
-        const celsiusValue = Math.floor((value-32)*5/9);
+        const celsiusValue = Math.floor((value - 32) * 5 / 9);
         return `${celsiusValue}°C`;
     });
 }
-function trimTemp(objArray){
-   
-return objArray.map((obj)=>{
-    obj.temperature = obj.temperature.replace(/\s+/g, '')
-    return obj
-});
+function trimTemp(objArray) {
+
+    return objArray.map((obj) => {
+        obj.temperature = obj.temperature.replace(/\s+/g, '')
+        return obj
+    });
 }
 
-function tempForecasts(objArray){
+function tempForecasts(objArray) {
     return objArray.map(obj => {
         const celc = parseFloat(obj.temperature.trim());
-        const celsiusValue = Math.floor((celc-32)*5/9);
+        const celsiusValue = Math.floor((celc - 32) * 5 / 9);
         const stateValue = obj.state
             .split(' ')
             .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
