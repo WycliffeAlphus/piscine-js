9b6fe33ebfd9e7b6e93877408fd8bda9c3bfe02a
wyonyango <wycliffealphus7@gmail.com>
2024-09-05 08:24:30 +0300

    add filter file

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 function filter(array, func){
 let filtered = [];
 for (let i=0; i < array.length; i++){
-    if (func(array[i], i , arr)){
+    if (func(array[i], i , array)){
         filtered.push(array[i]);
     }
 }
@@ -11,7 +11,7 @@ return filtered;
 function reject(array, func){
     let rejected = [];
     for (let i=0; i<array.length; i++){
-        if (!func(array[i], i, arr)){
+        if (!func(array[i], i, array)){
             rejected.push(arr[i]);
         }
     }
@@ -22,7 +22,7 @@ function partition(array, func){
     let passed = [];
     let rejected = [];
     for (let i=0; i<array.length;i++){
-        if (func(arr[i], i, arr)){
+        if (func(arr[i], i, array)){
             passed.push(arr[i]);
         } else {
             rejected.push(arr[i]);
