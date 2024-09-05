6d3427cccbbd9782c621de428f867c6ccafe1076
wyonyango <wycliffealphus7@gmail.com>
2024-09-05 09:00:40 +0300

    feat: add reduc.js file

--- reduce.js
+++ reduce.js
@@ -1,37 +1,38 @@
-function fold(arr, func, initial){
-let acc = initial;
-for ( let i =0; i<arr.legth; i++){
-    acc = func(acc, arr[i]);
-}
-return acc;
-}
-
-function foldRight(arr, func, initial){
+function fold(arr, func, initial) {
     let acc = initial;
-    for(let i=arr.length; i>=0;i--){
-        acc = func(acc, arr[i]);
+    for (let i = 0; i < arr.length; i++) {
+      acc = func(acc, arr[i]);
     }
     return acc;
-}
-
-function reduce(arr, func){
+  }
+  
+  function foldRight(arr, func, initial) {
+    let acc = initial;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      acc = func(acc, arr[i]);
+    }
+    return acc;
+  }
+  
+  function reduce(arr, func) {
     if (arr.length < 1) {
-        return "Error:arry is empty";
+     throw new Error("Array is empty");
     }
-    let acc = arr[0]; 
-    for(let i=1; i<arr.length;i++){
-        acc = func(acc, arr[i]);
-    }
-    return acc;
-}
-
-function reduceRight(arr, func){
-    if (arr.length < 1){
-        return "Error:array is empty";
-    }
-    let acc = arr[arr.length-1];
-    for (let i = arr.length-2; i>=0; i--){
-        acc = func(acc, arr[i]);
+    let acc = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      acc = func(acc, arr[i]);
     }
     return acc;
-}
+  }
+  
+  function reduceRight(arr, func) {
+    if (arr.length < 1) {
+        throw new Error("Array is empty");
+    }
+    let acc = arr[arr.length - 1];
+    for (let i = arr.length - 2; i >= 0; i--) {
+      acc = func(acc, arr[i]);
+    }
+    return acc;
+  }
+  
