fd76ba9a1b66b2753289394a1b64f8a13837e107
wyonyango <wycliffealphus7@gmail.com>
2024-09-07 09:53:28 +0300

    feat:add get-them-all.js

---
+++ get-them-all.js
@@ -0,0 +1,23 @@
+export const getArchitects = () => {
+    const architects = document.querySelectorAll('body a');
+    const nonArchitects = document.querySelectorAll('body > :not(a)')
+    return [architects, nonArchitects];
+};
+
+export const getClassical = () => {
+    const classical = document.querySelectorAll('body .classical') 
+    const nonClassical = document.querySelectorAll('body > :not(.classical')
+    return [classical, nonClassical]
+}
+
+export const getActive = () => {
+    const active = document.querySelectorAll('body .classical.active')
+    const nonClassical = document.querySelectorAll('body > .classical :not(.active)')
+    return [active, nonClassical]
+}
+
+export const getBonannoPisano = () => {
+    const bonano = document.querySelectorAll('body #BonannoPisano')
+    const notBonano = document.querySelectorAll('body > .classical.active)')
+    return [bonano, notBonano]
+}
