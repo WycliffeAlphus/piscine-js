cd756d38ebaa547edca16568f5fe35705ba89e6a
wyonyango <wycliffealphus7@gmail.com>
2024-09-07 09:57:22 +0300

    feat:add get-them-all.js

--- get-them-all.js
+++ get-them-all.js
@@ -1,23 +1,23 @@
 export const getArchitects = () => {
     const architects = document.querySelectorAll('body a');
-    const nonArchitects = document.querySelectorAll('body > :not(a)')
+    const nonArchitects = document.querySelectorAll('body > :not(a)');
     return [architects, nonArchitects];
 };
 
 export const getClassical = () => {
-    const classical = document.querySelectorAll('body .classical') 
-    const nonClassical = document.querySelectorAll('body > :not(.classical')
-    return [classical, nonClassical]
+    const classical = document.querySelectorAll('body .classical'); 
+    const nonClassical = document.querySelectorAll('body > :not(.classical)'); 
+    return [classical, nonClassical];
 }
 
 export const getActive = () => {
-    const active = document.querySelectorAll('body .classical.active')
-    const nonClassical = document.querySelectorAll('body > .classical :not(.active)')
-    return [active, nonClassical]
+    const active = document.querySelectorAll('body .classical.active');
+    const nonActive = document.querySelectorAll('body .classical:not(.active)'); 
+    return [active, nonActive];
 }
 
 export const getBonannoPisano = () => {
-    const bonano = document.querySelectorAll('body #BonannoPisano')
-    const notBonano = document.querySelectorAll('body > .classical.active)')
-    return [bonano, notBonano]
+    const bonano = document.querySelectorAll('body #BonannoPisano');
+    const notBonano = document.querySelectorAll('body > :not(#BonannoPisano)');
+    return [bonano, notBonano];
 }
