5b7c767b2be6e194743dc87aad3011a10aaf5407
wyonyango <wycliffealphus7@gmail.com>
2024-09-07 14:24:28 +0300

    feat:add pimp-my-style.js

---
+++ pimp-my-style.js
@@ -0,0 +1,31 @@
+import {styles} from "./pimp-my-style.data.js"
+let addMode = true;  
+let currentIndex = 0;  
+export function pimp() {
+    const button = document.querySelector('.button');
+
+    if (!addMode) {
+        button.classList.toggle('unpimp');
+        if (currentIndex > 0) {
+            currentIndex--;
+            button.classList.remove(styles[currentIndex]);
+        }
+        
+        if (currentIndex === 0) {
+            addMode = true;  
+        }
+        button.classList.toggle('unpimp');
+        return;
+    }
+
+    if (currentIndex < styles.length) {
+        button.classList.add(styles[currentIndex]);
+        currentIndex++;
+    }
+
+    if (currentIndex === styles.length) {
+        addMode = false;  
+    }
+}
+
+document.addEventListener('click', pimp);
