59ab44b571783239883f744b15d985cd13acbc52
wyonyango <wycliffealphus7@gmail.com>
2024-09-07 13:25:23 +0300

    feat:add harder-bigger-bolder-stronger.js

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -26,5 +26,9 @@ export function generateLetters() {
     }
 }
 
-generateLetters();
+const random = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
 
