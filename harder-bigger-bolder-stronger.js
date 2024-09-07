ba563824a3be8308649605ef233ca3e4da0b2f31
wyonyango <wycliffealphus7@gmail.com>
2024-09-07 12:51:21 +0300

    feat:add harder-bigger-bolder-stronger.js

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,30 @@
+
+function generateLetters() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    const container = document.createElement('div');
+
+    for (let i = 0; i < 120; i++) {
+       
+        const letterDiv = document.createElement("div");
+
+        
+        const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+        
+        letterDiv.textContent = randomLetter;
+ const fontSize = 11 + ((130 - 11) / 119) * i; 
+        letterDiv.style.fontSize = `${fontSize}px`;
+
+        
+        if (i < 40) {
+            letterDiv.style.fontWeight = '300';
+        } else if (i < 80) {
+            letterDiv.style.fontWeight = '400';
+        } else {
+            letterDiv.style.fontWeight = '600';
+        }
+
+        
+        container.appendChild(letterDiv);
+    }
+}
