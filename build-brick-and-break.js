f730aab15b5d761e934945fbdadaa0042f0975af
wyonyango <wycliffealphus7@gmail.com>
2024-09-07 11:46:27 +0300

    feat:refactor code

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,27 +1,28 @@
 function build(nb) {
     let count = 0;
-    
+
     const interval = setInterval(() => {
         if (count >= nb) {
             clearInterval(interval); 
             return;
         }
 
-       
+        
         const brick = document.createElement("div");
 
        
         brick.id = `brick-${count + 1}`;
 
-        
-        if ((count + 1) % 3 === 0) {  
+      
+        if ((count + 1) % 3 === 0) {
             brick.setAttribute('foundation', 'true');
         }
-       
+
+      
         document.body.appendChild(brick);
 
         count++;
-    }, 100);  
+    }, 100); 
 }
 
 function repair(...ids){
