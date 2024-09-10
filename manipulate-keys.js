73fac818fcbf4c64748145c2aa8f57636dfdad43
wyonyango <wycliffealphus7@gmail.com>
2024-09-10 16:33:20 +0300

    feat:add manipulate-keys

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -24,23 +24,28 @@ for (let i=0; i<keys.length; i++){
 return res;
 }
 
-function reduceKeys(obj, callback, initialValue){
-   let accumulator = initialValue !== undefined? initialValue:"";
-    let res = "";
+function reduceKeys(obj, callback, accumulator){
     let keys = Object.keys(obj)
     for (let i=0; i<keys.length; i++){
         if (obj.hasOwnProperty(keys[i])){
-            if (accumulator==':'){
-                accumulator += callback(accumulator, keys[i]);
-                return accumulator;
+            if (typeof accumulator === 'undefined') {
+                accumulator = keys[i]
+                continue
             }
-            if (res === ""){
-                res += keys[i]
-            } else {
-                res += callback(accumulator, keys[i]);
-            }
+
+         accumulator = callback(accumulator, keys[i]);
           
         }
     }
-    return res;
+    return accumulator;
 }
+let cart = {
+    vinegar: 80,
+    sugar: 100,
+    oil: 50,
+    onion: 200,
+    garlic: 22,
+    paprika: 4,
+  }
+ 
+
