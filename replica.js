f1bdb1232cf4b9627032b4eb0f0114b819b6c3e6
wyonyango <wycliffealphus7@gmail.com>
2024-09-12 08:25:36 +0300

    feat:add replica.js

---
+++ replica.js
@@ -0,0 +1,30 @@
+// Create a function named replica that allows you to deep assign the values of all properties from one or more objects to a target object.
+
+
+const is = {
+    arr: (n) => Array.isArray(n),
+    obj: (n) => typeof n === 'object' && n !== null && !(n instanceof RegExp) && !Array.isArray(n),
+    fun: (n) => typeof n === 'function'
+};
+
+function replica(target, ...sources) {
+    sources.forEach((source) => {
+        Object.keys(source).forEach((key) => {
+            if (is.obj(source[key])) {
+                if (!is.obj(target[key])) {
+                    target[key] = {};
+                }
+                replica(target[key], source[key]);
+            } else if (is.arr(source[key])) {
+                target[key] = source[key].map(item => 
+                    is.obj(item) || is.arr(item) ? replica({}, item) : item
+                );
+            } else if (source[key] instanceof RegExp) {
+                target[key] = new RegExp(source[key]);
+            } else {
+                target[key] = source[key];
+            }
+        });
+    });
+    return target;
+}
