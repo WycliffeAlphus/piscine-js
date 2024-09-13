92031572d5f629d416da7913c2170859d8a1da33
Wycliffe Onyango <wyonyango@noreply.learn.zone01kisumu.ke>
2024-09-13 04:52:19 +0000

    Update 'debounce.js'

--- debounce.js
+++ debounce.js
@@ -10,24 +10,26 @@ function debounce(func, wait){
     };
 }
 
-function opDebounce(func, wait, leading = false){
-    let timeout;
-    let lastCalled = false;
+function opDebounce(func, wait, leading = false) {
+  let timeout;
+  let lastCalled = false; // To track if the leading call was made
+  
+  return function(...args) {
+    const callNow = leading && !timeout;
 
-    return function(args){
-        const callNow = leading && !timeout;
+    const later = () => {
+      timeout = null;
+      if (!leading || lastCalled) {
+        func.apply(this, args);
+      }
+    };
 
-        const later = () => {
-            timeout = null;
-            if (!leading || lastCalled){
-                func.apply(this, args);
-            }
-        };
-        clearTimeout(timeout);
-       
-        if(callNow) {
-            func.apply(this, args);
-            lastCalled =true;
-        }
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+
+    if (callNow) {
+      func.apply(this, args);
+      lastCalled = true;
     }
+  };
 }
