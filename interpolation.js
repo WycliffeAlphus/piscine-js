083db5d0f1791fb5d62b35af6dc6f498394da50b
wyonyango <wycliffealphus7@gmail.com>
2024-09-14 09:37:18 +0300

    feat:add interpolation.js

--- interpolation.js
+++ interpolation.js
@@ -1,19 +1,17 @@
-function interpolation({step, start, end, callback, duration}){
-    const stepSize = (end-start)/step;
-    const delay = duration/step;
+function interpolation(obj) {
+    let step = obj.step
+    let start = obj.start
+    let end = obj.end
+    let callback = obj.callback
+    let duration = obj.duration
+    let interval = duration / step
+    let decrement = (end - start) / step
+      for (let i = 0; i < step; i++) {
+        let distance = start + (i * decrement)
+        let time = (i + 1) * interval
 
- // runStep runs each step with a delay   
-   function runStep(i) {
-    if (i >= step)return Promise.resolve();
-
-    return new Promise((resolve)=>{
-        setTimeout(()=> {
-            const distance = i;
-            const point = start+stepSize*i;
-            callback([distance, point]);
-            resolve();
-        }, delay*i);
-    }).then(() => runStep(i+1));
-   }
-   return runStep(0);
+        setTimeout(() => {
+            callback([distance, time])
+        }, time)
+    }
 }
