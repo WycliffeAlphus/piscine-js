6ae041d73c4d5db721aee3b6102e224cdd228aa2
wyonyango <wycliffealphus7@gmail.com>
2024-09-14 16:38:30 +0300

    feat:add get-json.js

--- get-json.js
+++ get-json.js
@@ -1,24 +1,25 @@
-function get(obj, path) {
-    const keys = path.split('.');
-
-    return keys.reduce((acc, key) => {
-        if (acc !== undefined && acc !== null) {
-            // Check if the key is an array index
-            if (!isNaN(key)) {
-                key = parseInt(key, 10);
-            }
-            // Check if the key is a method call
-            if (typeof acc[key] === 'function') {
-                return acc[key];
-            }
-            return acc[key];
-        }
-        return undefined;
-    }, obj);
+async function getJSON(path = "", params = {}) {
+	const url =
+		path +
+		"?" +
+		Object.keys(params)
+			.map((key) => {
+				return (
+					key.replace(" ", "+") + "=" + params[key].toString().replace(" ", "+")
+				);
+			})
+			.join("&");
+	const res = await fetch(url).then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error(response.statusText);
+		}
+	});
+	if (res.error) {
+		throw new Error(res.error);
+	}
+	return res.data;
 }
 
-// Example usage
-const src = { a: [{ b: "t" }] };
-const path = "a.0.b.toString";
 
-console.log(get(src, path)); // Output: "t"
