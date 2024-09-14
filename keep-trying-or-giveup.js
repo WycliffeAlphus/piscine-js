de395e45b713ffa02d7217985490a3e7065771b0
wyonyango <wycliffealphus7@gmail.com>
2024-09-14 10:14:09 +0300

    feat:add keep-trying-or-giveup.js

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,27 @@
+function retry(count, callback){
+    return async function(...args){
+        let attempts = 0;
+        while (attempts < count){
+            try {
+                return await callback(...args)
+            } catch(error){
+                attempts++;
+
+                if (attempts >= count) {
+                    throw new Error(`Failed after ${count} attempts; ${error.message}`)
+                }
+            }
+        }
+    }
+}
+
+
+function timeout(delay, callback){
+    return async function(...args){
+        const timeoutPromise = new Promise((_, reject) =>
+        setTimeout(() => reject(new Error('timeout')), delay));
+
+        const callbackPromise = callback(...args);
+        return Promise.race([callbackPromise, timeoutPromise])
+    };
+}
