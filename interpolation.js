f6f360229012e783f55d275343be9b35c8d81998
wyonyango <wycliffealphus7@gmail.com>
2024-09-14 09:19:24 +0300

    feat:add interpolation file

--- interpolation.js
+++ interpolation.js
@@ -1,12 +1,23 @@
 function interpolation({step, start, end, callback, duration}){
     const stepSize = (end-start)/step;
-    const delay = delay/step;
+    const delay = duration/step;
 
-    for (let i=0; i<step; i++){
-        setTimeout(()=>{
+ // runStep runs each step with a delay   
+   function runStep(i) {
+    return new Promise((resolve)=>{
+        setTimeout(()=> {
             const distance = i;
-            const point = start +stepSize * i
+            const point = start+stepSize*i;
             callback([distance, point]);
+            resolve();
         }, delay*i);
-    }
+    });
+   }
+
+ // promise create array of Promises for each step
+ const promises = [];
+ for (let i=0; i<step; i++){
+    promises.push(runStep(i));
+ }
+return Promise.all(promises);
 }
