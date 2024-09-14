c7661d76dadb9efd05da956bdcd9a4c76553de6f
wyonyango <wycliffealphus7@gmail.com>
2024-09-14 16:48:12 +0300

    feat: add series.js

---
+++ series.js
@@ -0,0 +1,9 @@
+async function series(args) {
+    let results = [];
+    for (const fn of args) {
+      const result = await fn(); 
+      results.push(result); 
+    }
+    return results;
+  }
+  
