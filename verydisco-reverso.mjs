793f672c87b77473ef747a6b84a35ec70f109b94
wyonyango <wycliffealphus7@gmail.com>
2024-09-17 12:38:39 +0300

    feat:add reversing logic

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -8,7 +8,7 @@ const fileReader = async filename => {
 
         for (let i = 0; i < content.length; i++) {
             if (content[i] === " ") { 
-                let parts = Math.ceil(temp.length / 2);
+                let parts = Math.floor(temp.length / 2);
                 let part1 = temp.slice(0, parts);
                 let part2 = temp.slice(parts);
                 res += part2 + part1 + " ";
